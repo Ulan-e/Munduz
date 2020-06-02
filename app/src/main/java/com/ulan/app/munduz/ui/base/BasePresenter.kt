26f7b16d68b746eba3a271e2de26a20298b9184f
Ulan Erkinbaev <ulanerkinbaev@gmail.com>
2020-06-02 17:05:30 +0300

    Added bindView() & unbindView() to BasePresenter

--- app/src/main/java/com/ulan/app/munduz/ui/base/BasePresenter.kt
+++ app/src/main/java/com/ulan/app/munduz/ui/base/BasePresenter.kt
@@ -1,7 +1,33 @@
 package com.ulan.app.munduz.ui.base
 
-interface BasePresenter {
+import android.util.Log
+import androidx.annotation.CallSuper
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 
-    fun detachView()
+abstract class Presenter<V> {
 
+    @Nullable
+    private var view: V? = null
+
+    protected fun getView(): V? {
+        Log.d("ulanbek", "getView() $view")
+        return view
+    }
+
+    @CallSuper
+    open fun bindView(@NonNull view: V) {
+        this.view = view
+        Log.d("ulanbek", "bindView() $view")
+    }
+
+    @CallSuper
+    open fun unbindView(@NonNull view: V) {
+        if (this.view == view) {
+            this.view = null
+        } else {
+            throw IllegalStateException("Unexpected view")
+        }
+        Log.d("ulanbek", "unbindView() $view")
+    }
 }
