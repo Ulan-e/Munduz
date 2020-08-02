f18b5e69e1d00d3b3e4bc5781e356e864ba14508
Dev <devu1@gmail.com>
2020-08-02 22:11:59 +0300

    Add In-app update API

--- app/src/main/java/ulanapp/munduz/ui/activities/main/MainActivity.kt
+++ app/src/main/java/ulanapp/munduz/ui/activities/main/MainActivity.kt
@@ -2,14 +2,21 @@ package ulanapp.munduz.ui.activities.main
 
 import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender.SendIntentException
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.InstallStatus.DOWNLOADED
+import com.google.android.play.core.install.model.InstallStatus.INSTALLED
+import com.google.android.play.core.install.model.UpdateAvailability.UPDATE_AVAILABLE
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import ulanapp.munduz.R
@@ -21,6 +28,7 @@ import ulanapp.munduz.helpers.Constants.Companion.FAVORITE_FRAGMENT
 import ulanapp.munduz.helpers.Constants.Companion.HOME_FRAGMENT
 import ulanapp.munduz.helpers.Constants.Companion.MORE_FRAGMENT
 import ulanapp.munduz.helpers.Constants.Companion.OPEN_BASKET_ARG
+import ulanapp.munduz.helpers.Constants.Companion.TAG
 import ulanapp.munduz.ui.activities.search.SearchActivity
 import ulanapp.munduz.ui.base.BaseActivity
 import ulanapp.munduz.ui.base.BaseFragment
@@ -31,6 +39,7 @@ import ulanapp.munduz.ui.fragments.home.HomeFragment
 import ulanapp.munduz.ui.fragments.more.MoreFragment
 import javax.inject.Inject
 
+
 class MainActivity : BaseActivity(), MainView {
 
     @Inject
@@ -38,45 +47,22 @@ class MainActivity : BaseActivity(), MainView {
 
     private lateinit var appUpdateManager: AppUpdateManager
 
-    companion object{
+    companion object {
         const val REQUEST_UPDATE_CODE = 13
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        appUpdateManager = AppUpdateManagerFactory.create(this)
-
-        val title = intent.getStringExtra(EXTRA_OPEN_BASKET)
-
-        presenter.bindView(this)
-
-        if (title == OPEN_BASKET_ARG) {
-            initBottomNav(R.id.basket)
-        } else {
-            initBottomNav(R.id.home)
-        }
-
-        button_click.setOnClickListener {
-            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_UPDATE_CODE){
-            if(resultCode != Activity.RESULT_OK){
-                Log.e(Constants.TAG, "Update flow failed! Result code: $resultCode")
+    private var installStateUpdatedListener: InstallStateUpdatedListener =
+        object : InstallStateUpdatedListener {
+            override fun onStateUpdate(state: InstallState) {
+                if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                    popupMessageForCompleteUpdate()
+                } else if (state.installStatus() == INSTALLED) {
+                    appUpdateManager.unregisterListener(this)
+                } else {
+                    Log.i(TAG, "InstallStateUpdatedListener: state: " + state.installStatus())
+                }
             }
         }
-    }
-
-    override fun initBottomNav(id: Int) {
-        bottom_navigation_menu.setOnNavigationItemSelectedListener(itemSelectedListener)
-        bottom_navigation_menu.selectedItemId = id
-    }
 
     private var itemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
@@ -110,6 +96,73 @@ class MainActivity : BaseActivity(), MainView {
             false
         }
 
+    override fun onStart() {
+        super.onStart()
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        appUpdateManager.registerListener(installStateUpdatedListener)
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() === UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                try {
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this@MainActivity,
+                        REQUEST_UPDATE_CODE
+                    )
+                } catch (e: SendIntentException) {
+                    e.printStackTrace()
+                }
+            } else if (appUpdateInfo.installStatus() === DOWNLOADED) {
+                popupMessageForCompleteUpdate()
+            } else {
+                Log.e(TAG, "checkForAppUpdateAvailability: something else")
+            }
+        }
+    }
+
+    private fun popupMessageForCompleteUpdate() {
+        val snackMessage = Snackbar.make(
+            main_activity_root,
+            "Приложение обновлено",
+            Snackbar.LENGTH_INDEFINITE
+        )
+
+        snackMessage.setAction("Установить") {
+            appUpdateManager.completeUpdate()
+        }
+
+        snackMessage.setActionTextColor(resources.getColor(R.color.green_dark))
+        snackMessage.show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val title = intent.getStringExtra(EXTRA_OPEN_BASKET)
+
+        presenter.bindView(this)
+
+        if (title == OPEN_BASKET_ARG) {
+            initBottomNav(R.id.basket)
+        } else {
+            initBottomNav(R.id.home)
+        }
+
+        button_click.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+        }
+    }
+
+    override fun initBottomNav(id: Int) {
+        bottom_navigation_menu.setOnNavigationItemSelectedListener(itemSelectedListener)
+        bottom_navigation_menu.selectedItemId = id
+    }
+
     override fun showFragment(fragment: BaseFragment, title: String) {
         supportFragmentManager
             .beginTransaction()
@@ -119,13 +172,11 @@ class MainActivity : BaseActivity(), MainView {
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if(appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)){
-                
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_UPDATE_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+                Log.e(Constants.TAG, "Update flow failed! Result code: $resultCode")
             }
         }
     }
@@ -142,6 +193,11 @@ class MainActivity : BaseActivity(), MainView {
                 fragment.onBackPressed()
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        appUpdateManager.unregisterListener(installStateUpdatedListener)
     }
 
     override fun onDestroy() {
